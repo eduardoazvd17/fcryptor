3fcb3408401df00c958d7176aaba8448ed12999a
Eduardo Azevedo <eduardoazvd17@gmail.com>
2025-01-14 21:12:07 -0300

    Creating a extension to file short name

--- lib/utils/file_extension.dart
+++ lib/utils/file_extension.dart
@@ -3,4 +3,15 @@ import 'dart:io';
 extension FileExtension on File {
   String get name =>
       path.replaceAll(parent.path, '').replaceAll('/', '').replaceAll('\\', '');
+
+  String get shortName {
+    final splittedName = this.name.split('.');
+    final extension = splittedName.last;
+    final name = splittedName.join('.').replaceAll('.$extension', '');
+
+    if (name.length > 15) {
+      return '${name.substring(0, 8)}...${name.substring(name.length - 7)}.$extension';
+    }
+    return this.name;
+  }
 }
