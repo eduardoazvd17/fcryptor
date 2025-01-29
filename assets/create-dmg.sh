8e04dc707ef00f87aa0e5234f7492951b14b2ff1
Eduardo Azevedo <eduardoazvd17@gmail.com>
2025-01-29 14:29:11 -0300

    Saving command to create dmg

---
+++ assets/create-dmg.sh
@@ -0,0 +1,1 @@
+create-dmg --window-pos 200 120 --window-size 800 400 --icon-size 100 --icon "fcryptor.app" 200 190 --hide-extension "fcryptor.app" --hide-extension "fcryptor.app" --app-drop-link 600 185 --volname "FCryptor for MacOS" "fcryptor-macos.dmg" "FCryptor/"
