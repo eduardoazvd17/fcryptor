e30f7c8219aeeafacbdb7ccc2f6803cd5013b60c
Eduardo Azevedo <eduardoazvd17@gmail.com>
2025-02-24 17:23:00 -0300

    Update macOS DMG creation script path

--- assets/create-dmg.sh
+++ assets/create-dmg.sh
@@ -1,1 +1,1 @@
-create-dmg --window-pos 200 120 --window-size 800 400 --icon-size 100 --icon "fcryptor.app" 200 190 --hide-extension "fcryptor.app" --hide-extension "fcryptor.app" --app-drop-link 600 185 --volname "FCryptor for MacOS" "fcryptor-macos.dmg" "FCryptor/"
+create-dmg --window-pos 200 120 --window-size 800 400 --icon-size 100 --icon "fcryptor.app" 200 190 --hide-extension "fcryptor.app" --hide-extension "fcryptor.app" --app-drop-link 600 185 --volname "FCryptor for MacOS" "fcryptor-macos.dmg" "../build/macos/Build/Products/Release"
