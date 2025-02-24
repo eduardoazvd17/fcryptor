d354101093ba51b148a53ebfba1bb3691468af18
Eduardo Azevedo <eduardoazvd17@gmail.com>
2025-02-24 16:45:41 -0300

    New deploy to github pages

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4209206717"
+    serviceWorkerVersion: "3390317138"
   }
 });
